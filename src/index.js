f00461fc15dadc39c88437db5a3297a13da2fe1d
Iago Cavalcante <iago.cavalcante.94@gmail.com>
2022-07-05 12:00:27 -0400

    Primeiro Componente

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import './index.css'
 import ReactDom from 'react-dom'
 import React from 'react' // usada para importar a biblioteca react
 
+import Primeiro from './components/basicos/Primeiro'
+
 const tag =  "Olá React !!"
 
 ReactDom.render(
     // inicio da sintaxe JSX da biblioteca react
     <div>
         { tag }
+        <Primeiro></Primeiro>
     </div>,
     // fim da sintaxe JSX da biblioteca react
     document.getElementById('root')
