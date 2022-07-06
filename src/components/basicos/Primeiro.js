755056725d60bc5dc8b327c9f9dc376dac0ccf1e
Iago Cavalcante <iago.cavalcante.94@gmail.com>
2022-07-05 21:20:12 -0400

    Primeiro Componente com JSX

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,11 @@
+import React from 'react' //bibioteca necessária para adicionar o JSX
+
 export default function Primeiro() {
-    return 'Primeiro Componente'
+    const msg = "Seja Bem Vindo(a)!!"
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p> { msg } </p>
+        </div> //adicionado o JSX
+    ) //usando estrutura de retorno com vários elementos a serem retornados
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import React from 'react' // usada para importar a biblioteca react
 
 import Primeiro from './components/basicos/Primeiro'
 
-const tag =  "Olá React !!"
-
 ReactDom.render(
     // inicio da sintaxe JSX da biblioteca react
     <div>
-        { tag }
         <Primeiro></Primeiro>
     </div>,
     // fim da sintaxe JSX da biblioteca react
